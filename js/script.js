3fff364b7aa1711379099558cd29b56fdff9a1ce
Dark <turkmvc@gmail.com>
2023-11-04 15:39:12 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* typing animation */
+var typed = new Typed('.typing', {
+    strings:[".Net Web Developper","FullStack .Net Developper", "Backend .Net Developper", "Angular Developper"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
